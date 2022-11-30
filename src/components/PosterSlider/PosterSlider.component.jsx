c75b3e0d6a0755964869372fe9130f0f351acc27
iam-arpitjain <jainarpit762@gmail.com>
2022-11-30 18:59:16 +0530

    Completed movie navbar and movie info section

--- src/components/PosterSlider/PosterSlider.component.jsx
+++ src/components/PosterSlider/PosterSlider.component.jsx
@@ -46,11 +46,16 @@ const PosterSlider = (props) => {
                 <h3 className={`text-2xl font-bold ${isDark ? 'text-white' : 'text-black'}`} >{title}</h3>
                 <p className={`text-sm ${isDark ? 'text-white' : 'text-gray-800'}`} >{subtitle}</p>
             </div>
-            <Slider {...settings}>
+            {config && (<Slider {...config}>
                 {
                     posters.map((each, index) => <Poster {...each} isDark={isDark} key={index} />)
                 }
-            </Slider>
+            </Slider>)}
+            {!config && (<Slider {...settings}>
+                {
+                    posters.map((each, index) => <Poster {...each} isDark={isDark} key={index} />)
+                }
+            </Slider>)}
         </>
     )
 };
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -6,12 +6,13 @@ import { MovieContext } from '../context/Movie.context';
 import Slider from 'react-slick';
 import { FaCcVisa, FaCcApplePay } from 'react-icons/fa';
 import PosterSlider from '../components/PosterSlider/PosterSlider.component';
+import MovieHero from '../components/MovieHero/MovieHero.component';
 
 const MoviePage = () => {
 
     const { id } = useParams();
 
-    const { movie } = useContext(MovieContext);
+    const { movie, setMovie } = useContext(MovieContext);
 
     const [cast, setCast] = useState([]);
     const [similarMovies, setSimilarMovies] = useState([]);
@@ -44,14 +45,53 @@ const MoviePage = () => {
         requestRecommendedMovies();
     }, [id]);
 
+    useEffect(() => {
+        const requestMovie = async () => {
+            const getMovieData = await axios.get(`/movie/${id}`);
+            setMovie(getMovieData.data);
+        };
+
+        requestMovie();
+    }, [id]);
+
     const settingsCast = {};
 
-    const settings = {};
+    const settings = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                },
+            },
+        ]
+    };
 
     return (
         <>
-            {/* <MovieHero /> */}
-            <div className='my-12 container px-4 lg:w-2/1'>
+            <MovieHero />
+            <div className='my-12 container px-4 lg:w-2/3'>
                 <div className='flex flex-col items-start gap-3'>
                     <h1 className='text-gray-800 font-bold text-2xl'> About the Movie</h1>
                     <p>{movie.overview}</p>
@@ -63,7 +103,7 @@ const MoviePage = () => {
 
                 <div className='my-8'>
                     <h2 className='text-gray-800 font-bold text-2xl mb-3'> Applicable Offers </h2>
-                    <div className='flex flex-col gap-3 lg:flex-row lg:w-3/4'>
+                    <div className='flex flex-col gap-3 lg:flex-row'>
                         <div className='flex items-start gap-2 bg-yellow-100 p-3 border-yellow-400 border-dashed border-2 rounded-md'>
                             <div className='w-8 h-8'><FaCcVisa className='w-full h-full' /></div>
                             <div className='flex flex-col items-start'>
